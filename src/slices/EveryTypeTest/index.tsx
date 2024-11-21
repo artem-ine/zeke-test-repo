7488faee2fc913e2fb44c3be4cabaa17613706ce
zeke <ezekiel.ravidat@gmail.com>
2024-11-21 15:53:14 +0100

    display every test slice

--- src/slices/EveryTypeTest/index.tsx
+++ src/slices/EveryTypeTest/index.tsx
@@ -1,6 +1,8 @@
 import { Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import styles from "./index.module.css";
+
 
 /**
  * Props for `EveryTypeTest`.
@@ -16,27 +18,28 @@ const EveryTypeTest = ({ slice }: EveryTypeTestProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={styles.everytypetest}
     >
-      <PrismicNextImage field={slice.primary.image} />
-      {slice.primary.text}
-      <PrismicRichText field={slice.primary.rich_text} />
-      <PrismicNextLink field={slice.primary.link} />
-      {slice.primary.select}
-      {slice.primary.boolean}
-      {slice.primary.number}
-      {slice.primary.color}
-      {slice.primary.date}
-      {slice.primary.timestamp}
-      {slice.primary.geopoint.latitude}, {slice.primary.geopoint.longitude}
-      <PrismicNextLink field={slice.primary.link_to_media} />
-      {slice.primary.repeatable_group?.map((item, index) => (
-        item.repeated_rich_text && (
-          <PrismicRichText 
-            key={index} 
-            field={item.repeated_rich_text} 
-          />
-        )
-      ))}
+        <PrismicNextImage field={slice.primary.image} />
+        {slice.primary.text}
+        <PrismicRichText field={slice.primary.rich_text} />
+        <PrismicNextLink field={slice.primary.link} />
+        <p>{slice.primary.select}</p>
+        <p>{slice.primary.boolean}</p>
+        <p>{slice.primary.number}</p>
+        <p>{slice.primary.color}</p>
+        <p>{slice.primary.date}</p>
+        <p>{slice.primary.timestamp}</p>
+        <p>{slice.primary.geopoint.latitude}, {slice.primary.geopoint.longitude}</p>
+        <PrismicNextLink field={slice.primary.link_to_media} />
+        {slice.primary.repeatable_group?.map((item, index) => (
+          item.repeated_rich_text && (
+            <PrismicRichText 
+              key={index} 
+              field={item.repeated_rich_text} 
+            />
+          )
+        ))}
     </section>
   );
 };
